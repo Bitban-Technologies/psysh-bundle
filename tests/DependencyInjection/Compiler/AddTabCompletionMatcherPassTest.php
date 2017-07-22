bbef56830cc0d1a44379f4c3c50f68814f3cf764
Alex Masterov <alex.masterow@gmail.com>
2017-07-22 15:18:57 +0300

    Improve AddTabCompletionMatcherPass test cases

--- tests/DependencyInjection/Compiler/AddTabCompletionMatcherPassTest.php
+++ tests/DependencyInjection/Compiler/AddTabCompletionMatcherPassTest.php
@@ -20,31 +20,58 @@ use Symfony\Component\DependencyInjection\{
 
 final class AddTabCompletionMatcherPassTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function it_valid_processed()
+    /** @test */
+    public function it_valid_processed_when_no_shell()
+    {
+        $container = $this->container();
+        $container->removeDefinition('psysh.shell');
+
+        // Execute
+        $container->compile();
+
+        // Verify
+        self::assertFalse($this->hasAddTabCompletionMatchersCall($container));
+
+        $container = $this->container();
+
+        // Execute
+        $container->compile();
+
+        // Verify
+        self::assertFalse($this->hasAddTabCompletionMatchersCall($container));
+    }
+
+    /** @test */
+    public function it_valid_processed_when_tagged()
     {
         $container = $this->container();
         $container->register(MongoClientMatcher::class)
             ->setAutoconfigured(true);
 
+        // Execute
         $container->compile();
 
+        // Verify
+        self::assertTrue($this->hasAddTabCompletionMatchersCall($container));
         self::assertContainsOnlyInstancesOf(
             MongoClientMatcher::class,
             $container->get('psysh.config')->getTabCompletionMatchers()
         );
     }
 
+    private function hasAddTabCompletionMatchersCall(ContainerBuilder $container): bool
+    {
+        return $container->getDefinition('psysh.config')
+            ->hasMethodCall('addTabCompletionMatchers');
+    }
+
     private function container(): ContainerBuilder
     {
         $container = new ContainerBuilder();
+        $container->addCompilerPass(new AddTabCompletionMatcherPass());
         $container->register('psysh.config', Configuration::class);
         $container->register('psysh.shell', Shell::class)
             ->addArgument(new Reference('psysh.config'));
-
-        $container->addCompilerPass(new AddTabCompletionMatcherPass());
         $container->registerForAutoconfiguration(AbstractMatcher::class)
             ->addTag('psysh.matcher');
 
