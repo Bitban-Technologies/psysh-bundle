010866a6e159295cb497a70518383e820cca3a01
Alex Masterov <alex.masterow@gmail.com>
2017-07-22 15:15:12 +0300

    Improve AddCommandPass test cases

--- tests/DependencyInjection/Compiler/AddCommandPassTest.php
+++ tests/DependencyInjection/Compiler/AddCommandPassTest.php
@@ -18,31 +18,58 @@ use Symfony\Component\DependencyInjection\{
 
 final class AddCommandPassTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function it_valid_processed()
+    /** @test */
+    public function it_valid_processed_then_no_shell()
+    {
+        $container = $this->container();
+        $container->removeDefinition('psysh.shell');
+
+        // Execute
+        $container->compile();
+
+        // Verify
+        self::assertFalse($this->hasAddCommandsCall($container));
+
+        $container = $this->container();
+
+        // Execute
+        $container->compile();
+
+        // Verify
+        self::assertFalse($this->hasAddCommandsCall($container));
+    }
+
+    /** @test */
+    public function it_valid_processed_when_tagged()
     {
         $container = $this->container();
         $container->register(TestCommand::class)
             ->setAutoconfigured(true);
 
+        // Execute
         $container->compile();
 
+        // Verify
+        self::assertTrue($this->hasAddCommandsCall($container));
         self::assertInstanceOf(
             TestCommand::class,
             $container->get('psysh.shell')->find('test')
         );
     }
 
+    private function hasAddCommandsCall(ContainerBuilder $container): bool
+    {
+        return $container->getDefinition('psysh.config')
+            ->hasMethodCall('addCommands');
+    }
+
     private function container(): ContainerBuilder
     {
         $container = new ContainerBuilder();
+        $container->addCompilerPass(new AddCommandPass());
         $container->register('psysh.config', Configuration::class);
         $container->register('psysh.shell', Shell::class)
             ->addArgument(new Reference('psysh.config'));
-
-        $container->addCompilerPass(new AddCommandPass());
         $container->registerForAutoconfiguration(Command::class)
             ->addTag('psysh.command');
 
